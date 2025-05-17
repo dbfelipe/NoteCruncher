c49fccd0beeecfe07e4c1d57b4939b897319759e
Daniel Felipe <dbfelipe24@gmail.com>
2025-05-17 09:59:08 -0500

    still debugging

--- backend/src/middleware/upload.middleware.js
+++ backend/src/middleware/upload.middleware.js
@@ -5,7 +5,7 @@ const path = require("path");
 // Store files in /uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads");
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
