24ad0abfd59848da6dac581bf22e0eab3d5b2d97
Daniel Felipe <dbfelipe24@gmail.com>
2025-05-21 16:03:57 -0500

    Added transcript column and now OpenAI api call for summarization is working

--- backend/src/models/schema.sql
+++ backend/src/models/schema.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS summaries (
     id SERIAL PRIMARY KEY,
-    video_id VARCHAR(255) NOT NULL UNIQUE,
-    video_url TEXT NOT NULL,
+    video_id VARCHAR(255) UNIQUE,
+    video_url TEXT,
     title VARCHAR(255) NOT NULL,
     summary TEXT NOT NULL,
+    transcript TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
