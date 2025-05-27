31318ab44d1fd1a8598948c7692ad17885757ef8
Daniel Felipe <dbfelipe24@gmail.com>
2025-05-27 14:39:56 -0500

    connecting frontend path

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
 
+const path = require("path");
+app.arguments(express.static(path.join(__dirname, "..", "pulic")));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
