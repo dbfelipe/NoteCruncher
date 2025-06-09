5efc902e1048521213a2bef0da33d8a03488c67f
Daniel Felipe <dbfelipe24@gmail.com>
2025-06-09 16:30:13 -0500

    fix: corrected static path and added missing express import

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 const app = require("./app");
-
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const path = require("path");
-app.arguments(express.static(path.join(__dirname, "..", "pulic")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
